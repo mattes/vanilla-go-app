335d3eb6f56bb3c778ac91842fb7e1e7134a617d
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2018-07-12 13:14:33 -0700

    add timeouts

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
+	"time"
 
 	"github.com/templarbit/vanilla-go-app/server"
 )
@@ -11,20 +13,51 @@ import (
 func main() {
 	listenFlag := flag.String("listen", ":8080", "")
 	httpKeepAliveFlag := flag.Bool("http-keep-alive", true, "")
+	httpReadTimeoutFlag := flag.Duration("http-read-timeout", 10*time.Second, "")
+	httpWriteTimeoutFlag := flag.Duration("http-write-timeout", 10*time.Second, "")
+	httpIdleTimeoutFlag := flag.Duration("http-idle-timeout", 30*time.Second, "")
+	tcpKeepAliveFlag := flag.Bool("tcp-keep-alive", true, "")
+	tcpIdleTimeoutFlag := flag.Duration("tcp-idle-timeout", 60*time.Second, "")
 
 	flag.Parse()
 
-	srv := &http.Server{
-		Addr:    *listenFlag,
-		Handler: server.Server(),
+	httpServer := &http.Server{
+		Handler:      server.Server(),
+		ReadTimeout:  *httpReadTimeoutFlag,
+		WriteTimeout: *httpWriteTimeoutFlag,
+		IdleTimeout:  *httpIdleTimeoutFlag,
 	}
 
-	// By default, keep-alives are always enabled
+	// By default, http keep-alives are enabled
 	// https://golang.org/pkg/net/http/#Server.SetKeepAlivesEnabled
 	if !*httpKeepAliveFlag {
-		srv.SetKeepAlivesEnabled(false)
+		httpServer.SetKeepAlivesEnabled(false)
 	}
 
-	log.Printf("Start listening on %v", *listenFlag)
-	log.Fatal(srv.ListenAndServe())
+	l, err := net.Listen("tcp", *listenFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Start listening", *listenFlag)
+	lt := TcpKeepAliveListener{l.(*net.TCPListener), *tcpKeepAliveFlag, *tcpIdleTimeoutFlag}
+	log.Fatal(httpServer.Serve(lt))
+}
+
+// TcpKeepAliveListener is more or less copied from:
+// https://github.com/golang/go/blob/release-branch.go1.10/src/net/http/server.go#L3211
+type TcpKeepAliveListener struct {
+	*net.TCPListener
+	KeepAlive bool
+	Timeout   time.Duration
+}
+
+func (ln TcpKeepAliveListener) Accept() (c net.Conn, err error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return
+	}
+	tc.SetKeepAlive(ln.KeepAlive)
+	tc.SetKeepAlivePeriod(ln.Timeout)
+	return tc, nil
 }
