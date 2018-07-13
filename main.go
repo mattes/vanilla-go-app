7cc02feec95c0e668e434c6e3240765932dd55ee
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2018-07-12 19:55:21 -0700

    add comments

--- main.go
+++ main.go
@@ -15,15 +15,16 @@ import (
 )
 
 func main() {
-	listenFlag := flag.String("listen", ":8080", "")
-	httpKeepAliveFlag := flag.Bool("http-keep-alive", true, "")
-	httpReadTimeoutFlag := flag.Duration("http-read-timeout", 10*time.Second, "")
-	httpWriteTimeoutFlag := flag.Duration("http-write-timeout", 10*time.Second, "")
-	httpIdleTimeoutFlag := flag.Duration("http-idle-timeout", 30*time.Second, "")
-	tcpKeepAliveFlag := flag.Bool("tcp-keep-alive", true, "")
-	tcpIdleTimeoutFlag := flag.Duration("tcp-idle-timeout", 60*time.Second, "")
-	connectionDrainingTimeoutFlag := flag.Duration("connection-draining-timeout", 30*time.Second, "")
-	shutdownDelayFlag := flag.Duration("shutdown-delay", 15*time.Second, "")
+	listenFlag := flag.String("listen", ":8080", "Listen for connections on host:port")
+	httpKeepAliveFlag := flag.Bool("http-keep-alive", true, "Enable HTTP KeepAlive")
+	httpReadTimeoutFlag := flag.Duration("http-read-timeout", 10*time.Second, "ReadTimeout is the maximum duration for reading the entire request, including the body.")
+	httpWriteTimeoutFlag := flag.Duration("http-write-timeout", 10*time.Second, "WriteTimeout is the maximum duration before timing out writes of the response.")
+	httpIdleTimeoutFlag := flag.Duration("http-idle-timeout", 30*time.Second, "IdleTimeout is the maximum amount of time to wait for the next request when keep-alives are enabled.")
+	tcpKeepAliveFlag := flag.Bool("tcp-keep-alive", true, "Enable TCP KeepAlive")
+	tcpIdleTimeoutFlag := flag.Duration("tcp-idle-timeout", 60*time.Second, "Set <duration> TCP KeepAlive Timeout")
+	connectionDrainingTimeoutFlag := flag.Duration("connection-draining-timeout", 30*time.Second, "Allow <duration> to gracefully shutdown existing connections")
+	shutdownDelayFlag := flag.Duration("shutdown-delay", 15*time.Second, "After SIGINT or SIGTERM is received, wait <duration> before no more new connections are accepted")
+	allowForcedShutdownFlag := flag.Bool("allow-forced-shutdown", true, "If second SIGINT or SIGTERM is received, forcefully shutdown immediatly.")
 
 	flag.Parse()
 
@@ -61,16 +62,24 @@ func main() {
 	<-shutdown
 	log.Println("Shutting down ...")
 
+	if *allowForcedShutdownFlag {
+		go func() {
+			<-shutdown
+			log.Fatal("Forced shutdown!")
+		}()
+	}
+
 	// 1. Delay shutdown
 	// This is useful when run inside Docker container with Kubernetes as
 	// scheduler for example: Kubernetes will send SIGTERM to let container
-	// know end of life is coming soon, but expect the container to still serve
+	// know that end of life is coming soon, but expect the container to still serve
 	// HTTP request until Kubernetes has updated all proxies and the container
 	// is finally removed.
 	time.Sleep(*shutdownDelayFlag)
 
 	// 2. Close all open listeners
 	// 3. Close all idle connections
+
 	// 4. Wait up to connectionDrainingTimeoutFlag for existing connections
 	//    to return to idle and then shut down
 	// 5. After connectionDrainingTimeoutFlag, forcefully shutdown
@@ -79,6 +88,8 @@ func main() {
 	defer cancel()
 	err = httpServer.Shutdown(ctx)
 	if err != nil {
+		// will log error if there were still existing connections
+		// after timeout has passed
 		log.Fatal(err)
 	}
 
